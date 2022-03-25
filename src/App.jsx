36de1857119e0e247d404dec42a8cbe72905bacb
Ianina Nazarenko <ynzrnk@yandex.ru>
2022-03-25 10:42:26 +0300

    Router 6 & Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { Landing, Register, Dashboard, Error } from './pages'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Jobify App</h1>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
